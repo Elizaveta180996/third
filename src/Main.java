862a925b9b9bb2c4e500e21ff2722d1b12e92d84
Elizaveta Anokhova <liza.ru96@mail.ru>
2023-07-27 23:46:00 +0300

    taskdone

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 4700; // сколько стоит билет
+        int ruble = 20; // рублей за одну милю
+
+
+
+        System.out.println("Купили билет, с одного билета получили:");
+        System.out.println((ticket / ruble) + " бонусных миль");
+
+}
+}
